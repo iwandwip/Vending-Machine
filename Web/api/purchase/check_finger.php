08edb4507bb0707976de204c6f14a73fe71afa67
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2022-10-10 17:00:16 +0700

    Update

--- Web/api/purchase/check_finger.php
+++ Web/api/purchase/check_finger.php
@@ -4,11 +4,11 @@ require_once "../../func.php";
 
 $finger_id = 0;
 
-if (isset($_GET["fingerid"])){
+if (isset($_GET["fingerid"])) {
 
     $finger_id = $_GET["fingerid"];
 
-    if ($finger_id == getUserID($finger_id, "id_pelanggan", $koneksi, "id_fingger")){
+    if ($finger_id == getUserID($finger_id, "id_pelanggan", $koneksi, "id_fingger")) {
 
         echo '1';
     }
